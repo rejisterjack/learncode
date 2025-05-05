0dcec3684b04a3f29e0647007fbf52a63be1a9fe
Rupam Das <rejisterjack@gmail.com>
2025-05-05 22:40:39 +0530

    prisma generations

---
+++ learncode-server/prisma/migrations/20250505162544_dev/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Problen` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Problen" DROP CONSTRAINT "Problen_userId_fkey";
+
+-- DropTable
+DROP TABLE "Problen";
+
+-- CreateTable
+CREATE TABLE "Problem" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "difficulty" "Difficulty" NOT NULL,
+    "tags" TEXT[],
+    "userId" TEXT NOT NULL,
+    "examples" JSONB NOT NULL,
+    "constraints" TEXT NOT NULL,
+    "hints" TEXT,
+    "editorial" TEXT,
+    "testCases" JSONB NOT NULL,
+    "codeSnippets" JSONB NOT NULL,
+    "referenceSolutions" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Problem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Problem" ADD CONSTRAINT "Problem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- learncode-server/src/app.ts
+++ learncode-server/src/app.ts
@@ -1,14 +1,17 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import { authRouter } from "./routes/auth.routes"
-import { problemRouter } from "./routes/problem.routes"
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import { authRouter } from './routes/auth.routes'
+import { problemRouter } from './routes/problem.routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/problems", problemRouter)
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the API' })
+})
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/problems', problemRouter)
 
 export default app
